f02c5975392cdf3654c86d1956ca272ea0cb97f7
Maciej Bartos <maciej.bartos@microsoft.wsei.edu.pl>
2022-03-13 11:00:36 +0100

    Initial commit

---
+++ ConsoleApp1/Ex1,Ex2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public void addText()
+        {
+ // ex. 1 ex. 2
+            string path = @"D:\Users\maciej.bartos\source\repos\myTxt.txt";
+
+        Console.WriteLine("podaj tekst");
+            string text = Console.ReadLine();
+            using (FileStream fs = File.Create(path))
+            {
+                AddText(fs, text);
+
+}
+
+//Open the stream and read it back.
+using (FileStream fs = File.OpenRead(path))
+{
+    byte[] b = new byte[1024];
+    UTF8Encoding temp = new UTF8Encoding(true);
+    while (fs.Read(b, 0, b.Length) > 0)
+    {
+        Console.WriteLine(temp.GetString(b));
+    }
+}
+        }
+ 
+
+        private static void AddText(FileStream fs, string value)
+{
+    byte[] info = new UTF8Encoding(true).GetBytes(value);
+    fs.Write(info, 0, info.Length);
+}        
+}
+    }
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static object binarySearch(int[] array, int value)
+        {
+            int index = 0;
+            int limit = array.Length -1;
+
+            while (index <= limit)
+            {
+                decimal val = (index + limit) / 2;
+                int point = (int)Math.Ceiling(val);
+                var entry = array[point];
+                if(value < entry)
+                {
+                    limit = point - 1;
+                    continue;
+                }
+                return point ;
+            }
+            return -1;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 4, 5, 7, 11, 12, 15, 15, 21, 40, 45 };
+            Console.WriteLine(binarySearch(arr, 11));
+            Console.WriteLine(Array.BinarySearch(arr, 11));    
+        }
+    }
+}
